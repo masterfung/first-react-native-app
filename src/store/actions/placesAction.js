f0afb080ef603c3c65be9288444030b2071943f7
Tsung Hung <hungmasterj@gmail.com>
2018-04-29 09:10:45 -0500

    redux setup

---
+++ src/store/actions/placesAction.js
@@ -0,0 +1,28 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes'
+
+export const addPlace = (placeName) => {
+  return {
+    type: ADD_PLACE,
+    placeName: placeName
+  };
+}
+
+export const deletePlace = (key) => {
+  return {
+    type: DELETE_PLACE,
+    key: key //need to double check
+  };
+}
+
+export const selectPlace = (key) => {
+  return {
+    type: SELECT_PLACE,
+    key: key
+  };
+}
+
+export const deselectPlace = () => {
+  return {
+    type: SELECT_PLACE
+  };
+}
